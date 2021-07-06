2921e4cbfb221e9ed61278099e54dc3bfb8a4e5c
timebear000 <timebear000@gmail.com>
2021-07-06 13:27:17 +0900

    인강완료

--- src/main/java/hello/itemservice/domain/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/domain/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -57,11 +58,13 @@ public class BasicItemController {
     }
 
     @PostMapping("/add")
-    public String addItem2(@ModelAttribute("item") Item item, Model model ){
+    public String addItem2(@ModelAttribute("item") Item item, RedirectAttributes redirectAttributes){
 
-        itemRepository.save(item);
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId",savedItem.getId());
+        redirectAttributes.addAttribute("status",true);
 //        model.addAttribute("item",item); //자동 추가 되기 떄문에 , 생략 가능
-        return "basic/item";
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
